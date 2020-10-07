fde5b2f582efaa025035ce44026447ecbc38073d
Melissa F <melissa.faulner@gmail.com>
2020-10-07 13:39:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
   array = [2, 4, 6, 8]
-  array.each{|number| puts number ** 2}
+  array.each do |number| 
+    puts number ** 2
+  end
+  new_array = array
+  new_array.each do |number|
+    puts number ** 2
+  end
 end
-
